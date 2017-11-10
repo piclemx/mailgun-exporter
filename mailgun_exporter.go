b406c00da8f1224cf69c52261e3b02203e1bfa81
Alexandre Picard-Lemieux <alexandre.lemieux@ticketmaster.com>
2017-11-10 13:27:35 -0500

    Little start for the mailgun exporter

---
+++ mailgun_exporter.go
@@ -0,0 +1,73 @@
+// Copyright 2015 The Prometheus Authors
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// A simple example exposing fictional RPC latencies with different types of
+// random distributions (uniform, normal, and exponential) as Prometheus
+// metrics.
+package main
+
+import (
+	"flag"
+	"math"
+	"math/rand"
+	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/common/log"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+    "gopkg.in/mailgun/mailgun-go.v1"
+	"fmt"
+)
+
+var (
+	addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+)
+
+
+func init() {
+	// Register the summary and the histogram with Prometheus's default registry.
+}
+
+func GetDomains(mg mailgun.Mailgun) (int, []mailgun.Domain, error) {
+	return mg.GetDomains(-1, -1)
+}
+
+func main() {
+	flag.Parse()
+
+	mg, error := mailgun.NewMailgunFromEnv()
+
+	if error != nil {
+		log.Fatal(error)
+	}
+
+	_ , domains , _ := GetDomains(mg)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Info("Number of domains for this account", nbDomains)
+
+	for index,element := range domains {
+
+		mg.GetStats()
+
+	}
+
+
+	// Expose the registered metrics via HTTP.
+	http.Handle("/metrics", promhttp.Handler())
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
